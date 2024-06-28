954891b2e20a7f6c5bde23d0ec8e403c14c629ec
José Manuel Piñeiro García <josempga@ext.inditex.com>
2024-06-28 16:24:28 +0100

    Add use effect without loop

--- src/use-effect.test.ts
+++ src/use-effect.test.ts
@@ -1,9 +1,13 @@
 import { describe, it, expect } from "vitest";
 import { renderHook, act } from "@testing-library/react";
-import { useEffectTest } from "./use-effect";
+import {
+  useEffectTest,
+  useEffectTestGoodSolutionUsingRef,
+  useEffectTestBadSolutionWithMissedDependencies,
+} from "./use-effect";
 
 describe("useEffectTest solutions", () => {
-  describe("useEffectTest", () => {
+  describe.skip("useEffectTest", () => {
     it("Should return empty array", () => {
       const { result } = renderHook(() => useEffectTest([]));
 
@@ -12,6 +16,54 @@ describe("useEffectTest solutions", () => {
       });
 
       expect(result.current.oneState).toStrictEqual(["pepito"]);
+    });
+  });
+  describe("useEffectTestBadSolutionWithMissedDependencies", () => {
+    it("Should return empty array", () => {
+      const { result, rerender } = renderHook(
+        useEffectTestBadSolutionWithMissedDependencies,
+        { initialProps: [] }
+      );
+
+      act(() => {
+        result.current.setOneAction(true);
+      });
+
+      expect(result.current.oneState).toStrictEqual(["pepito"]);
+
+      act(() => {
+        result.current.setOneAction(false);
+      });
+      act(() => {
+        rerender(["juanito"]);
+      });
+      act(() => {
+        result.current.setOneAction(true);
+      });
+      expect(result.current.oneState).toStrictEqual(["juanito", "pepito"]);
+    });
+  });
+  describe("useEffectTestGoodSolutionUsingRef", () => {
+    it("Should return empty array", () => {
+      const { result, rerender } = renderHook(
+        useEffectTestGoodSolutionUsingRef,
+        { initialProps: [] }
+      );
+
+      act(() => {
+        result.current.setOneAction(true);
+      });
+
+      expect(result.current.oneState).toStrictEqual(["pepito"]);
+
+      act(() => {
+        result.current.setOneAction(false);
+      });
+      rerender(["juanito"]);
+      act(() => {
+        result.current.setOneAction(true);
+      });
+      expect(result.current.oneState).toStrictEqual(["juanito", "pepito"]);
     });
   });
 });
--- src/use-effect.ts
+++ src/use-effect.ts
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from "react";
+import { useRef, useState, useCallback, useEffect } from "react";
 
 export const useEffectTest = (initialValues: Array<string>) => {
   const [oneState, setOneState] = useState<Array<string>>([]);
@@ -19,6 +19,63 @@ export const useEffectTest = (initialValues: Array<string>) => {
   useEffect(() => {
     handleAction(initialValues);
   }, [handleAction, initialValues]);
+
+  return { oneState, setOneAction };
+};
+
+export const useEffectTestGoodSolutionUsingRef = (
+  initialValues: Array<string>
+) => {
+  const [oneState, setOneState] = useState<Array<string>>([]);
+
+  const [oneAction, setOneAction] = useState<boolean>(false);
+
+  const handleAction = useCallback(
+    (values: Array<string>) => {
+      if (oneAction) {
+        setOneState([...values, "pepito"]);
+      } else {
+        setOneState([]);
+      }
+    },
+    [oneAction]
+  );
+
+  const initialValuesRef = useRef(initialValues);
+
+  useEffect(() => {
+    initialValuesRef.current = initialValues;
+  }, [initialValues]);
+
+  useEffect(() => {
+    handleAction(initialValuesRef.current);
+  }, [handleAction]);
+
+  return { oneState, setOneAction };
+};
+
+export const useEffectTestBadSolutionWithMissedDependencies = (
+  initialValues: Array<string>
+) => {
+  const [oneState, setOneState] = useState<Array<string>>([]);
+
+  const [oneAction, setOneAction] = useState<boolean>(false);
+
+  const handleAction = useCallback(
+    (values: Array<string>) => {
+      if (oneAction) {
+        setOneState([...values, "pepito"]);
+      } else {
+        setOneState([]);
+      }
+    },
+    [oneAction]
+  );
+
+  useEffect(() => {
+    handleAction(initialValues);
+    /* eslint-disable-next-line react-hooks/exhaustive-deps */
+  }, [handleAction]);
 
   return { oneState, setOneAction };
 };
